25d9e80934b72031993e2b8adfd52d7e11dc98da
Altan Simsir <altansimsir@outlook.com>
2023-02-04 13:29:31 +0300

    Initial Files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Patika.Dev Başlangıç Seviyesi .Net Core Patikası
+
+## C# 101 
+
+[Patika](www.patika.dev)
---
+++ console-programlama/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Merhabalar!");
+Console.WriteLine("Adiniz?");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadiniz?");
+string surname = Console.ReadLine();
+
+Console.WriteLine("Selamin aleykum " + name + " " + surname);
+
